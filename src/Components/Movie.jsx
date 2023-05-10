487fa1e9505a8a02fa7df6bf232739c3f81f402d
Luka Draskovic <lukadraskovic33@gmail.com>
2023-05-10 20:13:10 +0200

    fixes

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,90 +1,38 @@
 import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../index.css";
 
-function Movie({ movie, getMovieDetails }) {
-  const [showModal, setShowModal] = useState(false);
-  const [movieDetails, setMovieDetails] = useState(null);
+function Movie({ movie }) {
+  const [showDetails, setShowDetails] = useState(false);
 
-  const handleShowModal = async () => {
-    const details = await getMovieDetails(movie.imdbID);
-    setMovieDetails(details);
-    setShowModal(true);
+  const handleCardClick = () => {
+    setShowDetails(true);
   };
 
   return (
-    <>
-          <div className="card shadow-sm my-2 bg-transparent">
-            <img className="bd-placeholder-img card-img-top" width="100%" height="225px" src={movie.Poster}/>
-
-            <div className="card-body bg-transparent">
-              <p className="card-text text-white">{movie.Title.length > 20
-                  ? movie.Title.substring(0, 20) + "..."
-                  : movie.Title}</p>
-              <div className="d-flex justify-content-between align-items-center">
-                <div className="btn-group">
-                  <button type="button" className="btn btn-sm btn-outline-secondary">{movie.Type === "movie" ? "Movie" : "TV"}</button>
-                  <button type="button" className="btn btn-sm btn-outline-secondary" onClick={handleShowModal}>Show More</button>
-                </div>
-                <small className="text-muted text-white">{movie.Year}</small>
-              </div>
-            </div>
-          </div>
-
-      {movieDetails && (
-        <Modal show={showModal} onHide={() => setShowModal(false)} className="modal-transparent">
-          <Modal.Header closeButton>
-            <Modal.Title className="custom-dark">{movieDetails.Title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="d-flex justify-content-between">
-              <img
-                src={movieDetails.Poster}
-                alt={movieDetails.Title}
-                className="mr-4 modal-pic"
-              />
-              <div className="mx-5 h-100">
-                <p className=" border-bottom border-bottom-md-primary">
-                  Year: {movieDetails.Year}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Rated: {movieDetails.Rated}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Released: {movieDetails.Released}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Runtime: {movieDetails.Runtime}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Genre: {movieDetails.Genre}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Director: {movieDetails.Director}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Writer: {movieDetails.Writer}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Actors: {movieDetails.Actors}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Plot: {movieDetails.Plot}
-                </p>
-                <p className=" border-bottom border-bottom-md-primary">
-                  Rating: {movieDetails.imdbRating}
-                </p>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="dark" onClick={() => setShowModal(false)}>
-              Close
-            </Button>
-          </Modal.Footer>
-        </Modal>
+    <div className="card shadow-sm my-2 bg-transparent" onClick={handleCardClick}>
+      {showDetails ? (
+        <Link to={`/movie/${movie.imdbID}`}>
+          <img className="bd-placeholder-img card-img-top" width="100%" height="225px" src={movie.Poster} alt={movie.Title} />
+        </Link>
+      ) : (
+        <img className="bd-placeholder-img card-img-top" width="100%" height="225px" src={movie.Poster} alt={movie.Title} />
       )}
-    </>
+
+      <div className="card-body bg-transparent">
+        <p className="card-text text-white">
+          {movie.Title.length > 20 ? movie.Title.substring(0, 20) + "..." : movie.Title}
+        </p>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="btn-group">
+            <button type="button" className="btn btn-sm btn-outline-secondary">
+              {movie.Type === "movie" ? "Movie" : "TV"}
+            </button>
+          </div>
+          <small className="text-muted text-white">{movie.Year}</small>
+        </div>
+      </div>
+    </div>
   );
 }
 
